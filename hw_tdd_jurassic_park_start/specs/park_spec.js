a034ca7aed77004e1b61694d026e5f37a8e54ff9
Andrew Blair <blair.d.andrew@gmail.com>
2019-05-07 17:22:34 +0100

    two passed

--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -4,24 +4,31 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
 
-  beforeEach(function () {
+  let park;
 
+  beforeEach(function () {
+    park = new Park('Jurassic Park', 150, [])
   })
 
-  it('should have a name');
+    it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurassic Park');
+  });
+    it('should have a ticket price', function(){
+      const actual = park.ticketPrice;
+      assert.strictEqual(actual, 150);
+    });
 
-  it('should have a ticket price');
+    it('should have a collection of dinosaurs');
 
-  it('should have a collection of dinosaurs');
+    it('should be able to add a dinosaur to its collection');
 
-  it('should be able to add a dinosaur to its collection');
+    it('should be able to remove a dinosaur from its collection');
 
-  it('should be able to remove a dinosaur from its collection');
+    it('should be able to find the dinosaur that attracts the most visitors');
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+    it('should be able to find all dinosaurs of a particular species');
 
-  it('should be able to find all dinosaurs of a particular species');
-
-  it('should be able to remove all dinosaurs of a particular species');
+    it('should be able to remove all dinosaurs of a particular species');
 
 });
